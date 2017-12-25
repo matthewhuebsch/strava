affb46ad90fd519a614fd22c48b2032814b4d0cf
matthewhuebsch <matthew.huebsch+github@gmail.com>
2017-12-25 13:26:13 -0500

    Switch to Toronto coords.

--- 03_plot_run_map.R
+++ 03_plot_run_map.R
@@ -8,18 +8,24 @@ library(tidyverse)
 data <- readRDS("processed/data.RDS")
 
 # Specify lat / lon window
-lon_min <- 144.9
-lon_max <- 145.73
-lat_min <- -37.475
-lat_max <- -38.1
+lon_min <- -79.506569
+lon_max <- -79.288095
+lat_min <- 43.608261
+lat_max <- 43.730978
 
 # Create plot
 p <- ggplot() +
   geom_path(aes(lon, lat, group = id),
-            data %>% filter(lon > lon_min, lon < lon_max, lat < lat_min, lat > lat_max),
+            data %>% filter(lon > lon_min, lon < lon_max, lat < abs(lat_max), lat > abs(lat_min)),
             alpha = 0.3, size = 0.3, lineend = "round") +
   coord_equal() +
   theme_blankcanvas(margin_cm = 0)
 
 # Save plot
 ggsave("plots/map001.png", p, width = 20, height = 15, units = "cm", dpi = 600)
+
+# toronto info:
+# bottom left: 43.608261, -79.467152
+# top left: 43.684656, -79.506569
+# top right: 43.730978, -79.306468
+# bottom right: 43.635963, -79.288095
